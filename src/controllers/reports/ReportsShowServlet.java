37120f7d0871546615cb26313d5ca3bde006ea05
kosuke.miyazaki <miyazaki.kosuke307@gmail.com>
2020-07-03 10:56:03 +0900

    Add Report show

--- src/controllers/reports/ReportsShowServlet.java
+++ src/controllers/reports/ReportsShowServlet.java
@@ -2,23 +2,28 @@ package controllers.reports;
 
 import java.io.IOException;
 
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Report;
+import utils.DBUtil;
+
 /**
  * Servlet implementation class ReportsShoeServlet
  */
 @WebServlet("/reports/show")
-public class ReportsShoeServlet extends HttpServlet {
+public class ReportsShowServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ReportsShoeServlet() {
+    public ReportsShowServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -28,7 +33,18 @@ public class ReportsShoeServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+		EntityManager em = DBUtil.createEntityManager();
+
+		Report r = em.find(Report.class, Integer.parseInt(request.getParameter("id")));
+
+		em.close();
+
+		request.setAttribute("report", r);
+		request.setAttribute("_token", request.getSession().getId());
+
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/show.jsp");
+        rd.forward(request,response);
+
 	}
 
 }
